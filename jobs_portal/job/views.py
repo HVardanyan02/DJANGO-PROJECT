51b6fadb52f75c00c4f0ef591840a756d7bdc19c
Hayarpi <hayarpie.vardanyan@mail.ru>
2024-12-05 12:26:00 +0400

    login

--- jobs_portal/job/views.py
+++ jobs_portal/job/views.py
@@ -1,12 +1,59 @@
-from django.shortcuts import render
-from django.template.loader import render_to_string
+from django.shortcuts import render, redirect
+# from django.template.loader import render_to_string
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import *
 
 def index(request): 
     return render(request,'index.html',{})
 
-# def index(request):
-#     navbar_html = render_to_string('/navbar.html')
-#     return render(request, 'index.html', {'navbar_html': navbar_html})
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == "POST":
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            
+            if not User.objects.filter(username=email).exists():
+                messages.error(request, 'Invalid email')
+                return redirect('login')
+            
+            user = authenticate(username=email, password=password)
+            
+            if user is None:
+                messages.error(request, "Invalid Password")
+                return redirect('login')
+            else:
+                login(request, user)
+                return redirect('index')
+    
+    return render(request, 'login.html')
+
+def signUp(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        re_type_password = request.POST.get('re_type_password')
+        
+        if password != re_type_password:
+            messages.error(request, "Passwords do not match")
+            return redirect('login')
+        
+        if User.objects.filter(username=email).exists():
+            messages.error(request, "Email already registered")
+            return redirect('login')
+        
+        user = User.objects.create_user(username=email, password=password)
+        user.save()
+        
+        login(request, user)
+        messages.success(request, "Registration successful")
+        return redirect('index')
+    
+    return render(request, 'login.html', {})
 
 def about(request): 
     return render(request,'about.html',{})
@@ -23,8 +70,8 @@ def postJob(request):
 def blog(request): 
     return render(request,'blog.html',{})
 
-def login(request): 
-    return render(request,'login.html',{})
+# def login(request): 
+#     return render(request,'login.html',{})
 
 def blogSingle(request): 
     return render(request,'blog-single.html',{})
@@ -49,3 +96,7 @@ def portfolio(request):
 
 def portfolioSingle(request): 
     return render(request,'portfolio-single.html',{})
+
+
+
+
